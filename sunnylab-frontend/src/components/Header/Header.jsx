7ef08906ba559028610a4e6052aff50ac1d15460
BenjaBustosCC <ben.bustosm@duocuc.cl>
2025-12-23 16:05:34 -0300

    cambios al header

--- sunnylab-frontend/src/components/Header/Header.jsx
+++ sunnylab-frontend/src/components/Header/Header.jsx
@@ -1,56 +1,56 @@
 import './Header.css'
 import { Link } from "react-scroll";
 
-export function Header (){
-    return (
-        <header className='header'>
-            <div className='logo-container'>
-                <img className='header-logo' src="/logosl.png" alt="logo Sunny Lab" />
-            </div>
-            <div className='header-labels'>
-                <Link 
-                  to="hero" 
-                  smooth={true} 
-                  duration={100} 
-                  spy={true} 
-                  activeClass="active"
-                  className='header-label'
-                >
-                  Inicio
-                </Link>
-                <Link 
-                  to="services" 
-                  smooth={true} 
-                  duration={100} 
-                  spy={true} 
-                  activeClass="active"
-                  className='header-label'
-                >
-                  Servicios
-                </Link>
-                <Link 
-                  to="technical-specs" 
-                  smooth={true} 
-                  duration={100} 
-                  spy={true} 
-                  activeClass="active"
-                  className='header-label'
-                >
-                  Ficha técnica
-                </Link>
-                <Link 
-                  to="contact" 
-                  smooth={true} 
-                  duration={100}
-                  spy={true} 
-                  activeClass="active"
-                  className='header-label'
-                >
-                  Contacto
-                </Link>
-            </div>
-        </header>
-    )
+export function Header() {
+  return (
+    <header className='header'>
+      <div className='logo-container'>
+        <img className='header-logo' src="/SunnyLab/logosl.png" alt="logo Sunny Lab" />
+      </div>
+      <div className='header-labels'>
+        <Link
+          to="hero"
+          smooth={true}
+          duration={100}
+          spy={true}
+          activeClass="active"
+          className='header-label'
+        >
+          Inicio
+        </Link>
+        <Link
+          to="services"
+          smooth={true}
+          duration={100}
+          spy={true}
+          activeClass="active"
+          className='header-label'
+        >
+          Servicios
+        </Link>
+        <Link
+          to="technical-specs"
+          smooth={true}
+          duration={100}
+          spy={true}
+          activeClass="active"
+          className='header-label'
+        >
+          Ficha técnica
+        </Link>
+        <Link
+          to="contact"
+          smooth={true}
+          duration={100}
+          spy={true}
+          activeClass="active"
+          className='header-label'
+        >
+          Contacto
+        </Link>
+      </div>
+    </header>
+  )
 }
 
 export default Header
